dc7b4ed4c892b80840e2ebfe9e2686c37f4cb616
yzjflyzj <yanzhijie2014@163.com>
2022-07-05 23:22:52 +0800

    算法:动态规划:300. 最长递增子序列

---
+++ algorithms/leetcode/frame/dp_3.go
@@ -0,0 +1,20 @@
+package main
+
+import "sort"
+
+//300. 最长递增子序列
+func lengthOfLIS(nums []int) int {
+	//使得f按照升序排列
+	f := []int{}
+	for _, e := range nums {
+		//h := e
+		//golang内置的SearchInts函数已经实现了二分查找
+		//使用golang内置的SearchInts函数来查找当前队列中是否有，如果没有，返回此数应该被插入的位置。
+		if i := sort.SearchInts(f, e); i < len(f) {
+			f[i] = e
+		} else {
+			f = append(f, e)
+		}
+	}
+	return len(f)
+}
