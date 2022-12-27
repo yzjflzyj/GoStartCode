ac2f761b03e7cd4e2cf8f296b65f90753c815193
yanzhijie <yanzhijie2014@163.com>
2022-12-27 22:58:33 +0800

    错误处理:error,defer.panic,recover

--- demo6/main.go
+++ demo6/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
 
@@ -19,6 +20,42 @@ func main() {
 	} else {
 		fmt.Println(sum1)
 	}
+
+	//嵌套error
+	e := errors.New("原始错误e")
+	w := fmt.Errorf("Wrap了一个错误:%w", e)
+	fmt.Println(w)
+
+	//errors.Unwrap 函数
+	fmt.Println(errors.Unwrap(w))
+
+	//errors.Is 函数的定义，可以解释为：
+	//如果 err 和 target 是同一个，那么返回 true。
+	//如果 err 是一个 wrapping error，target 也包含在这个嵌套 error 链中的话，也返回 true。
+	fmt.Println(errors.Is(w, e))
+
+	//errors.As 函数
+	//有了嵌套之后，error的断言也失效了，因此使用errors.As
+	var cm *commonError
+	if errors.As(err, &cm) {
+		fmt.Println("错误代码为:", cm.errorCode, "，错误信息为：", cm.errorMsg)
+	} else {
+		fmt.Println(sum)
+	}
+
+	//defer关键字，保证后面的代码一定被执行，用来修饰方法或函数，常用于成对的操作
+
+	//panic函数，是go的内置函数，运行时的异常，可以接受 interface{} 类型的参数，也就是任何类型的值都可以传递给 panic 函数
+	//panic("手动抛出panic")
+	//panic 异常是一种非常严重的情况，会让程序中断运行，使程序崩溃，所以如果是不影响程序运行的错误，不要使用 panic，使用普通错误 error 即可。
+
+	//recover函数：配合defer关键字，从panic异常中恢复
+	defer func() {
+		if p := recover(); p != nil {
+			fmt.Println("恢复了panic异常", p)
+		}
+	}()
+	panic("手动抛出panic")
 }
 
 //主动抛异常
