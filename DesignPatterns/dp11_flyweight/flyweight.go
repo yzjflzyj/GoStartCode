037ef60e2c6339815a672a3e742831856f29aed5
yanzhijie <yanzhijie2014@163.com>
2023-01-16 22:06:21 +0800

    【设计模式】：享元

---
+++ DesignPatterns/dp11_flyweight/flyweight.go
@@ -0,0 +1,71 @@
+package main
+
+import "fmt"
+
+// ImageFlyweightFactory 工厂
+type ImageFlyweightFactory struct {
+	maps map[string]*ImageFlyweight
+}
+
+var imageFactory *ImageFlyweightFactory
+
+func GetImageFlyweightFactory() *ImageFlyweightFactory {
+	if imageFactory == nil {
+		imageFactory = &ImageFlyweightFactory{
+			maps: make(map[string]*ImageFlyweight),
+		}
+	}
+	return imageFactory
+}
+
+func (f *ImageFlyweightFactory) Get(filename string) *ImageFlyweight {
+	image := f.maps[filename]
+	if image == nil {
+		image = NewImageFlyweight(filename)
+		f.maps[filename] = image
+	}
+
+	return image
+}
+
+// ImageFlyweight 享元角色
+type ImageFlyweight struct {
+	data string
+}
+
+func NewImageFlyweight(filename string) *ImageFlyweight {
+	// Load image file
+	data := fmt.Sprintf("image data %s", filename)
+	return &ImageFlyweight{
+		data: data,
+	}
+}
+
+func (i *ImageFlyweight) Data() string {
+	return i.data
+}
+
+// ImageViewer 非享元角色
+type ImageViewer struct {
+	*ImageFlyweight
+}
+
+func NewImageViewer(filename string) *ImageViewer {
+	image := GetImageFlyweightFactory().Get(filename)
+	return &ImageViewer{
+		ImageFlyweight: image,
+	}
+}
+
+func (i *ImageViewer) Display() {
+	fmt.Printf("Display: %s\n", i.Data())
+}
+
+func main() {
+	viewer1 := NewImageViewer("image1.png")
+	viewer2 := NewImageViewer("image1.png")
+	if viewer1.ImageFlyweight == viewer2.ImageFlyweight {
+		fmt.Println("享元相同")
+	}
+	viewer1.Display()
+}
