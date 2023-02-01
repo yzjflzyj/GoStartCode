f52b50192dcf267a2a8b0126d359b892b7c86ff4
yanzhijie <yanzhijie2014@163.com>
2023-02-02 00:21:31 +0800

    算法：二叉树中序,后序遍历，DFS：106. 从中序与后序遍历序列构造二叉树

---
+++ algorithms/leetcode/frame/Binary_8.go
@@ -0,0 +1,32 @@
+package main
+
+/**
+ * 106. 从中序与后序遍历序列构造二叉树
+ */
+func buildTree2(inorder []int, postorder []int) *TreeNode {
+	if len(inorder) < 1 || len(postorder) < 1 {
+		return nil
+	}
+	//先找到根节点（后续遍历的最后一个就是根节点）
+	nodeValue := postorder[len(postorder)-1]
+	//从中序遍历中找到一分为二的点，左边为左子树，右边为右子树
+	left := findRootIndex2(inorder, nodeValue)
+	//构造root
+	root := &TreeNode{Val: nodeValue,
+		Left:  buildTree2(inorder[:left], postorder[:left]), //将后续遍历一分为二，左边为左子树，右边为右子树
+		Right: buildTree2(inorder[left+1:], postorder[left:len(postorder)-1])}
+	return root
+}
+
+func findRootIndex2(inorder []int, target int) (index int) {
+	for i := 0; i < len(inorder); i++ {
+		if target == inorder[i] {
+			return i
+		}
+	}
+	return -1
+}
+
+func main() {
+
+}
