b70292dc06a1798d5e05badd456dc07174ae53a4
yanzhijie <yanzhijie2014@163.com>
2023-02-04 16:54:57 +0800

    算法：单调栈：739.每日温度

--- algorithms/leetcode/frame/stack_3.go
+++ algorithms/leetcode/frame/stack_3.go
@@ -42,6 +42,31 @@ func getNextGreaterElement(nums []int) []int {
 	}
 	return res
 }
-func main() {
 
+/**
+  739.每日温度
+  * 给定一个整数数组temperatures，表示每天的温度，返回一个数组answer，
+  * 其中answer[i]是指对于第 i 天，下一个更高温度出现在几天后。如果气温在这之后都不会升高，请在该位置用0 来代替。
+*/
+//单调栈,只是栈中记录的是索引
+func dailyTemperatures(temperatures []int) []int {
+	size := len(temperatures)
+	res := make([]int, size)
+	var stack []int
+	for i := size - 1; i >= 0; i-- {
+		for len(stack) > 0 && temperatures[stack[len(stack)-1]] <= temperatures[i] {
+			stack = stack[:len(stack)-1]
+		}
+		if len(stack) > 0 {
+			res[i] = stack[len(stack)-1] - i
+		} else {
+			res[i] = 0
+		}
+		stack = append(stack, i)
+	}
+	return res
+}
+
+func main() {
+	dailyTemperatures([]int{89, 62, 70, 58, 47, 47, 46, 76, 100, 70})
 }
