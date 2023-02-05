c1de1b369eb6629c9932f3313b8d98a3834516e0
yanzhijie <yanzhijie2014@163.com>
2023-02-05 22:51:40 +0800

    算法：链表，双指针：21. 合并两个有序链表

---
+++ algorithms/leetcode/frame/Link_1.go
@@ -0,0 +1,31 @@
+package main
+
+/**
+ * 21. 合并两个有序链表
+ * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+ */
+//双指针
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	head := &ListNode{}
+	cur := head
+	for list1 != nil && list2 != nil {
+		if list1.Val < list2.Val {
+			cur.Next = list1
+			list1 = list1.Next
+		} else {
+			cur.Next = list2
+			list2 = list2.Next
+		}
+		cur = cur.Next
+	}
+	if list1 == nil {
+		cur.Next = list2
+	} else {
+		cur.Next = list1
+	}
+	return head.Next
+}
+
+func main() {
+
+}
