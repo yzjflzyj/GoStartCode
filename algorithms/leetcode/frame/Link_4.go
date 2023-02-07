2c9ea3560ae62d19587796e00830db778c09408d
yanzhijie <yanzhijie2014@163.com>
2023-02-08 00:32:38 +0800

    算法：链表，快慢指针：876. 链表的中间结点

---
+++ algorithms/leetcode/frame/Link_4.go
@@ -0,0 +1,21 @@
+package main
+
+/**
+ * 876. 链表的中间结点
+ * 给定一个头结点为 head 的非空单链表，返回链表的中间结点。
+ * 如果有两个中间结点，则返回第二个中间结点。
+ */
+//双指针，快慢指针
+func middleNode(head *ListNode) *ListNode {
+	slow := head
+	fast := head
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+	}
+	return slow
+}
+
+func main() {
+
+}
