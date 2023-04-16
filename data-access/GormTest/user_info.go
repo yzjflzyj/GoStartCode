75bdfebd98327747565a797fab79f25700965c9e
yanzhijie <yanzhijie2014@163.com>
2023-04-16 12:32:26 +0800

    gorm使用

--- data-access/GormTest/user_info.go
+++ data-access/GormTest/user_info.go
@@ -3,19 +3,21 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"gorm.io/gorm"
 	"time"
 )
 
 type User struct {
-	ID           uint
+	gorm.Model
 	Name         string
 	Email        *string
 	Age          uint8
 	Birthday     time.Time
 	MemberNumber sql.NullString
 	ActivatedAt  sql.NullTime
-	CreatedAt    time.Time
-	UpdatedAt    time.Time
+	//ID           uint
+	//CreatedAt    time.Time
+	//UpdatedAt    time.Time
 }
 
 type Album struct {
@@ -25,18 +27,36 @@ type Album struct {
 	Price  float32
 }
 
-func dbOperate() {
-	//user := User{Name: "Jinzhu", Age: 18, Birthday: time.Now()}
-	album := Album{Title: "Jinzhu", Artist: "周杰伦", Price: 123}
+func albumOperate() {
+	album := Album{Title: "song", Artist: "周杰伦", Price: 246}
 
 	result := db.Create(&album) // 通过数据的指针来创建
-	fmt.Println(result)
+	//album.ID             // 返回插入数据的主键
+	//result.Error        // 返回 error
+	//result.RowsAffected // 返回插入记录的条数
+	fmt.Println(result, album.ID, result.Error, result.RowsAffected)
+}
 
-	//nuser.ID             // 返回插入数据的主键
-	//nresult.Error        // 返回 error
-	//nresult.RowsAffected // 返回插入记录的条数
+func userAdd() {
+	users := []*User{
+		{Name: "Jinzhu", Age: 18, Birthday: time.Now()},
+	}
+	user := &User{Name: "Jackson", Age: 19, Birthday: time.Now()}
+	users = append(users, user)
+	result := db.Create(users) // pass a slice to insert multiple ROW
+	fmt.Println(result, len(users), result.Error, result.RowsAffected)
+
+	// 下面的批次创建，会开启事物
+	//db.CreateInBatches(users, 100)
+
+	// 指定字段创建
+	db.Select("Name", "Age", "CreatedAt").Create(&user)
+	// INSERT INTO `users` (`name`,`age`,`created_at`) VALUES ("Jackson", 19, "2023-04-06 11:05:21.775")
+	// 忽略字段创建
+	db.Omit("Name", "Age", "CreatedAt").Create(&user)
 }
 
 func main() {
-	dbOperate()
+	//albumOperate()
+	userAdd()
 }
