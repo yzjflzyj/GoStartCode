b2562980a207a0043974115950d9aa25bcdc98c7
yanzhijie <yanzhijie2014@163.com>
2023-04-16 23:43:47 +0800

    gorm使用

--- ginTest.go
+++ ginTest.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"GoStartCode/data-access/GormTest/models"
+	"GoStartCode/data-access/GormTest/pkg/setting"
 	"fmt"
 	"github.com/gin-gonic/gin"
 )
+
+func init() {
+	setting.Setup()
+	models.Setup()
+}
 
 func main() {
 	fmt.Println("先导入fmt包，才能使用")
